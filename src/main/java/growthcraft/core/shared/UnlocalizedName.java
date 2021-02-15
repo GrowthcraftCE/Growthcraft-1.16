a2327167c042d41d61430bace7ea208f7a420e79
Alatyami <alatyami@gmail.com>
2021-02-15 01:47:32 -0500

    Minor updates.

--- src/main/java/growthcraft/core/shared/UnlocalizedName.java
+++ src/main/java/growthcraft/core/shared/UnlocalizedName.java
@@ -2,16 +2,22 @@ package growthcraft.core.shared;
 
 public class UnlocalizedName {
 
+    // TODO[6]: Add unlocalized names for crowbars.
     public static final String CROWBAR_WHITE = "crowbar_white";
 
+    // TODO[]:
     public static final String SALT_ORE = "rocksalt_ore";
+    // TODO[]:
     public static final String SALT_BLOCK = "salt_block";
+    // TODO[]:
     public static final String SALT_ITEM = "salt";
 
+    // TODO[]:
     public static final String ROPE_LINEN = "rope_linen";
-
+    // TODO[]:
     public static final String ROPE_FENCE_OAK_LINEN = "rope_fence_oak_linen";
 
+    // TODO[*]: Move tags to lib
     public static final String TAG_ROPE = "rope";
     public static final String TAG_ROPE_FENCE = "rope_fence";
     public static final String TAG_HEATSOURCES = "heatsources";
