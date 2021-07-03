c9f99c634943b2f702b155d2af0dc88540c6956f
Alatyami <alatyami@gmail.com>
2021-07-03 14:45:08 -0400

    Progress #18 Added yeast items

--- src/main/java/growthcraft/cellar/shared/UnlocalizedName.java
+++ src/main/java/growthcraft/cellar/shared/UnlocalizedName.java
@@ -4,10 +4,20 @@ public class UnlocalizedName {
 
     public static final String BREW_KETTLE = "brew_kettle";
     public static final String BREW_KETTLE_LID = "brew_kettle_lid";
+    public static final String BREW_KETTLE_RECIPE = "brew_kettle_recipe";
     public static final String WORT = "wort";
     public static final String WORT_BUCKET = "wort_fluid_bucket";
     public static final String WORT_FLUID = "wort_fluid";
     public static final String WORT_FLUID_FLOWING = "wort_fluid_flowing";
+    public static final String YEAST_BAYANUS = "yeast_bayanus";
+    public static final String YEAST_BAYANUS_ETHEREAL = "yeast_bayanus_ethereal";
+    public static final String YEAST_BREWERS = "yeast_brewers";
+    public static final String YEAST_BREWERS_ETHEREAL = "yeast_brewers_ethereal";
+    public static final String YEAST_ETHEREAL = "yeast_ethereal";
+    public static final String YEAST_LAGER = "yeast_lager";
+    public static final String YEAST_LAGER_ETHEREAL = "yeast_lager_ethereal";
 
-    public static final String BREW_KETTLE_RECIPE = "brew_kettle_recipe";
+    private UnlocalizedName() {
+        /* Prevent generation of default public constructor */
+    }
 }
