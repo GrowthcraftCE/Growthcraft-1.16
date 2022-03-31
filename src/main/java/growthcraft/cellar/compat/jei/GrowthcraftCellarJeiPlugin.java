a6a298183394c75699c59662a2f54cf30abe1863
Alatyami <alatyami@gmail.com>
2022-03-30 20:27:30 -0600

    92 cheese press (#111)
    * Progress #92 Cheese Press base block completed
    
    * Fixed JEI Breakage some recipes had null outputs

--- src/main/java/growthcraft/cellar/compat/jei/GrowthcraftCellarJeiPlugin.java
+++ src/main/java/growthcraft/cellar/compat/jei/GrowthcraftCellarJeiPlugin.java
@@ -1,5 +1,6 @@
 package growthcraft.cellar.compat.jei;
 
+import growthcraft.cellar.GrowthcraftCellar;
 import growthcraft.cellar.compat.jei.category.*;
 import growthcraft.cellar.init.GrowthcraftCellarRecipes;
 import growthcraft.cellar.shared.Reference;
@@ -11,17 +12,21 @@ import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
+import org.apache.logging.log4j.Level;
 
 @JeiPlugin
 public class GrowthcraftCellarJeiPlugin implements IModPlugin {
 
     @Override
     public void registerIngredients(IModIngredientRegistration registry) {
+        GrowthcraftCellar.LOGGER.log(Level.DEBUG, "{} registering JEI ingredients.", Reference.MODID);
         IModPlugin.super.registerIngredients(registry);
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registry) {
+        GrowthcraftCellar.LOGGER.log(Level.DEBUG, "{} registering JEI Recipes.", Reference.MODID);
+
         // Brew Kettle Recipes
         registry.addRecipes(RecipeUtils.findRecipesByType(Minecraft.getInstance().world,
                 GrowthcraftCellarRecipes.BREW_KETTLE_RECIPE_TYPE), BrewKettleRecipeCategory.ID);
@@ -41,6 +46,8 @@ public class GrowthcraftCellarJeiPlugin implements IModPlugin {
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registry) {
+        GrowthcraftCellar.LOGGER.log(Level.DEBUG, "{} registering JEI categories.", Reference.MODID);
+
         // Register the recipe categories with JEI
         registry.addRecipeCategories(
                 new BrewKettleRecipeCategory(registry.getJeiHelpers().getGuiHelper()),
--- src/main/java/growthcraft/milk/common/recipe/MixingVatItemRecipe.java
+++ src/main/java/growthcraft/milk/common/recipe/MixingVatItemRecipe.java
@@ -65,4 +65,8 @@ public class MixingVatItemRecipe extends MixingVatRecipe {
         return this.resultActivationTool;
     }
 
+    @Override
+    public ItemStack getRecipeOutput() {
+        return this.getResultItemStack();
+    }
 }
