b1553dade4f0e23f2823ac846c943244013eceea
alatyami <alatyami@gmail.com>
2022-07-17 17:48:32 -0600

    Incremented version for bug fixes

--- src/main/java/growthcraft/core/shared/Reference.java
+++ src/main/java/growthcraft/core/shared/Reference.java
@@ -8,7 +8,7 @@ public class Reference {
 
     public static final String MODID = "growthcraft";
     public static final String NAME = "Growthcraft";
-    public static final String VERSION = "6.1.0";
+    public static final String VERSION = "6.1.1";
 
     public static final String CROWBAR_BLACK = "crowbar_black";
     public static final String CROWBAR_BLUE = "crowbar_blue";
